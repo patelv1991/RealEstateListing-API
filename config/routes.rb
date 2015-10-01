ec81d4f099dbbdbeb788d2551436b4075ec80a63
Varun Patel <patelv1991@gmail.com>
2015-09-30 17:47:09 -0700

    create listings migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-
+  resources :listings, only: [:index], defaults: { format: :json }
 end
--- db/migrate/20151001002825_create_listings.rb
+++ db/migrate/20151001002825_create_listings.rb
@@ -1,7 +1,14 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
-
+      t.string :street
+      t.string :status
+      t.integer :price
+      t.integer :bedrooms
+      t.integer :bathrooms
+      t.integer :sq_ft
+      t.float :lat
+      t.float :lng
       t.timestamps null: false
     end
   end
